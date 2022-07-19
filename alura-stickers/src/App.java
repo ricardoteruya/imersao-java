50b876f7f566d5e6948488f7ded01744637b7a21
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2022-07-19 08:56:43 -0300

    aponta para API alternativa

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -11,7 +11,8 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
