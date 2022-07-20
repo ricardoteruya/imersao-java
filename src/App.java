4e0c52db9fa26e008da5b83196275d91df81e28c
Ricardo <29868488+ricardoteruya@users.noreply.github.com>
2022-07-20 20:56:53 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		//TODO Consumir endpoint de API por 250 filmes mais assistidos
+		
+		//TODO Extrair informacoes relevates da requisicao
+		
+		//TODO Exibir nova listagem em stdout
+		
+	}
+
+}
