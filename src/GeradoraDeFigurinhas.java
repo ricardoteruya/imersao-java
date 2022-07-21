3bbac07055139c5a745e5a5d5c05d08b9a7274fb
Ricardo <29868488+ricardoteruya@users.noreply.github.com>
2022-07-21 19:03:19 -0300

    Criando lista de passos iniciais para aula 2

---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,16 @@
+
+public class GeradoraDeFigurinhas {
+	// leitura da imagem
+					
+	// cria nova imagem em memoria com transparencia e com tamanho novo
+	// identificar altura e largura
+	
+	// copiar a imagem original para nova imagem (em memoria)
+			
+	//configurar a fonte
+	
+	// escrever uma frase na nova imagem
+	
+	// escrever nova imagem em arquivo
+
+}
