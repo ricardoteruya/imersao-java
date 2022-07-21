1d1b7c1f1c31c0fca8315bb8278e279d42bbc2f5
Ricardo <29868488+ricardoteruya@users.noreply.github.com>
2022-07-21 13:30:29 -0300

    Aula 1 concluida Add classe de parser usada em video Consumindo endpoint de API fornecida por instrutores Extraindo valores de requisicao Exibindo Titulo, Imagem, Classificacao, em stdout dos filmes

--- src/App.java
+++ src/App.java
@@ -1,13 +1,33 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
 
-	public static void main(String[] args) {
-		//TODO Consumir endpoint de API por 250 filmes mais assistidos
+	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		//TODO Extrair informacoes relevates da requisicao
+		//Usando api alternativa fornecida pelos instrutores
+		String url = "https://alura-filmes.herokuapp.com/conteudos";
+		URI uri = URI.create(url);
+		HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
 		
-		//TODO Exibir nova listagem em stdout
+		HttpClient client = HttpClient.newHttpClient();
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		String body = response.body();
 		
+		JsonParser parser = new JsonParser();
+		List<Map<String,String>> listaDeFilmes = parser.parse(body);
+		
+		for (Map<String, String> filme : listaDeFilmes) {
+			System.out.println(filme.get("title"));
+			System.out.println(filme.get("image"));
+			System.out.println(filme.get("imDbRating"));		
+		}
 	}
 
 }
