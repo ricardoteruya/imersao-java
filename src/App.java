cd2fda474f5dab6cdf2729cd806874397ba7b379
Ricardo <29868488+ricardoteruya@users.noreply.github.com>
2022-07-22 11:36:44 -0300

    Alterando exibicao para baixar e modificar imagens das figurinhas.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,7 +11,7 @@ import java.util.Map;
 
 public class App {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws Exception {
 		
 		//Usando api alternativa fornecida pelos instrutores
 		String url = "https://alura-filmes.herokuapp.com/conteudos";
@@ -23,10 +25,22 @@ public class App {
 		JsonParser parser = new JsonParser();
 		List<Map<String,String>> listaDeFilmes = parser.parse(body);
 		
+		var geradora = new GeradoraDeFigurinhas();
+		String DIRETORIO = "saida";
 		for (Map<String, String> filme : listaDeFilmes) {
-			System.out.println(filme.get("title"));
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));		
+			String titulo = filme.get("title");
+			String nomeArquivo = DIRETORIO + "/" + titulo + ".png";
+			
+			String urlImagem = filme.get("image");			
+			InputStream inputStream = new URL(urlImagem).openStream();
+			
+			geradora.cria(inputStream, nomeArquivo);
+			System.out.println();
+			
+			
+//			System.out.println(filme.get("title"));
+//			System.out.println(filme.get("image"));
+//			System.out.println(filme.get("imDbRating"));
 		}
 	}
 
